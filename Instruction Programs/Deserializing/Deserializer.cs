e64e433f4071090a0c4ac6e74ef7a46c37dbdfd0
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-03-15 01:53:44 +0100

    Added program serializer and deserializer.

---
+++ Instruction Programs/Deserializing/Deserializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Rusty.Cutscenes;
+using Rusty.Csv;
+
+namespace Rusty.CutsceneImporter.InstructionPrograms
+{
+    /// <summary>
+    /// An deserializer for CSV-based cutscene instruction definitions.
+    /// </summary>
+    public static class Deserializer
+    {
+        /* Public methods. */
+        /// <summary>
+        /// Decompile a string of CSV into an instruction definition.
+        /// </summary>
+        public static CutsceneProgram Deserialize(CsvTable csv)
+        {
+            // Load instruction instances.
+            List<InstructionInstance> instructions = new();
+            for (int i = 0; i < csv.Height; i++)
+            {
+                string[] row = csv.GetRow(i);
+                string[] arguments = new string[row.Length - 1];
+                Array.Copy(row, 1, arguments, 0, arguments.Length);
+                instructions.Add(new(row[0], arguments));
+            }
+
+            // Create program.
+            return new(instructions.ToArray());
+        }
+    }
+}
